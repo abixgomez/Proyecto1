0d1acb3564c82b8869c4bc70a33e2d4f476f5cd9
xabigomezx <abigailg960@gmail.com>
2026-02-11 10:19:23 -0700

    proyecto 1

---
+++ src/main/java/Menu/MenuPrincipal.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Menu;
+
+import Persistencias.*;
+import Entidades.*;
+import java.util.Scanner;
+
+/**
+ *
+ * @author abigomez
+ */
+public class MenuPrincipal {
+    private IPersistenciaFachada persistencia;
+    private Scanner scanner;
+    
+    public MenuPrincipal(IPersistenciaFachada persistencia) {
+        this.persistencia = persistencia;
+    }
+    
+    
+}
